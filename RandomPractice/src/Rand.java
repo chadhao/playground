f60c2be7fc516ead0016fae68f6e2689482ef1e9
Hao Duan <haoduan@outlook.com>
2016-02-03 17:47:37 +1300

    update @ 5:47PM 03/02/2016

--- RandomPractice/src/Rand.java
+++ RandomPractice/src/Rand.java
@@ -1,22 +1,36 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Rand
 {
 	private static ArrayList<Integer> list;
 	private static long startTime;
 	private static long endTime;
+	private static Random rand;
 	
 	static
 	{
 		list = new ArrayList<>();
 		startTime = 0;
 		endTime = 0;
+		rand = new Random();
 	}
 	
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		initArray();
+		timeStart();
+		for (int i = 0; i < 1000000; i++)
+		{
+			int index = rand.nextInt(1000);
+			list.set(index, list.get(index)+1);
+		}
+		timeEnd();
+		System.out.println(endTime-startTime);
+		for (int i = 0; i < 1000; i++)
+		{
+			System.out.print(list.get(i) + " ");
+		}
 	}
 	
 	private static void timeStart()
